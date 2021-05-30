aefdf596e78ae50559cbd43b0fb65b6a00fafc57
MACGalaviz <macgalaviz@hotmail.com>
2021-05-30 08:43:13 -0500

    :sparkles: Add copy to clipboard functionality
    // Miguel Cabañas

--- src/containers/Notes/Current.js
+++ src/containers/Notes/Current.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 import { View } from 'react-native'
 import { Text, Card, Button } from 'react-native-elements'
@@ -16,6 +17,11 @@ class Current extends Component {
     dispatch(showModal({ open: false, type: 'current' }))
   }
 
+ handleCopyToClipboard = () => {
+    const { note } = this.props
+    Clipboard.setString(note.content);
+  };
+
   render() {
     const { note } = this.props
     const { content, checked } = note
@@ -28,6 +34,7 @@ class Current extends Component {
             <Text >{content}</Text>
           </View>
         <Card.Divider/>
+        { checked && <Button title="Copy" onPress={this.handleCopyToClipboard} /> }
         <Button title="Close" onPress={this.handleCloseModal} />
       </Card>
     )
